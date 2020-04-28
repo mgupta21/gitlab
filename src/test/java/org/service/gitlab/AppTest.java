554a88d3dd4f44f7d0385b9a11163956d324233a
mayankgupta <mayank.gupta1502@gmail.com>
2020-04-28 01:12:56 -0500

    initial setup

---
+++ src/test/java/org/service/gitlab/AppTest.java
@@ -0,0 +1,20 @@
+package org.service.gitlab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
